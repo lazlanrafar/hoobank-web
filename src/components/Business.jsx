e827fd1ce78892c5c6aa974a2fa86a38563baf14
lazlanrafar <lazlanrafar@gmail.com>
2022-11-30 13:43:33 +0700

    slicing business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
 
 export default function Business() {
+  const FeatureCard = ({ icon, title, content, isLast }) => {
+    return (
+      <div
+        className={`flex items-start ss:items-center p-6 rounded-[20px] ${
+          !isLast && "mb-6"
+        } feature-card`}
+      >
+        <div
+          className={`w-[50px] ss:w-[64px] h-[50px] ss:h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+        >
+          <img src={icon} alt="icon" className="w-1/2 h-1/2 object-contain" />
+        </div>
+        <div className="ml-5 flex-1 font-poppins text-white">
+          <h3 className="font-semibold text-lg">{title}</h3>
+          <p className={`${styles.paragraph} text-base`}>{content}</p>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <section id="features" className={`${layout.section}`}>
       <div className={`${layout.sectionInfo}`}>
@@ -17,6 +38,16 @@ export default function Business() {
         </p>
 
         <Button styles="mt-10" text="Get Started" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard
+            key={feature.id}
+            {...feature}
+            isLast={index == features.length - 1 ? true : false}
+          />
+        ))}
       </div>
     </section>
   );
